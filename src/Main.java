f039c5127aa0119defedb0f292ad9a818cf0e9f5
Megane <46535748+Megane@users.noreply.github.com>
2022-01-30 14:00:22 -0500

    classeLOCV1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import java.io.File;
+
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        //tests unitaires pour tester parsing
+        //TODO: cleanup at the end
+      /*  Parsing parstest = new Parsing();
+        File myObj = new File("src/test.txt");
+        parstest.classeLOC(myObj);
+        System.out.println("Nombres de lignes de code: " + parstest.classe_LOC);*/
     }
 }
