9dffa8608d141ca9e14e3b733f6ed82158029474
JulienThibeault <64616248+JulienThibeault@users.noreply.github.com>
2022-01-30 13:37:46 -0500

    TreeNode getters and setters

--- src/TreeNode.java
+++ src/TreeNode.java
@@ -1,22 +1,25 @@
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * TODO javadoc
+ * */
 public class TreeNode {
 
     private TreeNode parentNode;
     private Set<TreeNode> childNodes;
-    private String path;    // TODO : determine the kind of path we want here (partial or complete)
-    private String name;
+    private String path;    // TODO : determine the kind of path we want here (partial or complete, vs file and dir name)
 
-    // mesured metrics :
+    // mesured metrics
     private int loc;        // number of lines per class or package
     private int cloc;       // number of lines which include comments
-    private int wmc;        // weighted methods per class = number of methods per class
 
-    // calculated metrics :
+    // mesured (class) or calculated (package) metric
+    private int wmcwcp;     // number of methods per class or package
+
+    // calculated metrics
     private float dc;       // density of comments per class or package : dc = cloc / loc
-    private float bc;       // degree of comments for class or package : bc = dc / (wmc or wcp)
-    private int wcp;        // sum of wmc of class and subpackage for a package
+    private float bc;       // degree of comments for class or package : bc = dc / wmcwcp
 
     // Constructor to use for tree's root
     TreeNode () {
@@ -24,7 +27,7 @@ public class TreeNode {
         this.childNodes = new HashSet<>();
         this.loc = 0;
         this.cloc = 0;
-        this.wmc = 0;
+        this.wmcwcp = 0;
     }
 
     // Constructor to use for child node
@@ -33,17 +36,46 @@ public class TreeNode {
         this.childNodes = new HashSet<>();
         this.loc = 0;
         this.cloc = 0;
-        this.nmethods = 0;
+        this.wmcwcp = 0;
     }
 
+    // Setters & methods to update metrics recursively
 
-    // Methods
+    public void addLoc(int value) {
+        this.loc += value;
+        this.updateDc();
+        if (this.parentNode != null) {
+            this.parentNode.addLoc(this.loc);
+        }
+    }
 
-    // Getters and setters
+    public void addCloc(int value) {
+        this.cloc += value;
+        this.updateDc();
+        if (this.parentNode != null) {
+            this.parentNode.addCloc(this.cloc);
+        }
+    }
+
+    public void addWmcwcp(int value) {
+        this.wmcwcp += value;
+        this.updateBc();
+        if (this.parentNode != null) {
+            this.parentNode.addWmcwcp(this.wmcwcp);
+        }
+    }
 
     public void updateDc() {
-        float value = (float)this.cloc / (float)this.loc;
-        setDc(value);
+        if (this.loc != 0) {
+            this.dc = (float) this.cloc / (float) this.loc;
+            this.updateBc();
+        }
+    }
+
+    public void updateBc() {
+        if (this.wmcwcp != 0) {
+            this.bc = this.dc / (float) this.wmcwcp;
+        }
     }
 
     public void setParentNode(TreeNode parentNode) {
@@ -58,56 +90,38 @@ public class TreeNode {
         this.path = path;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    // Getters
+
+    public TreeNode getParentNode() {
+        return parentNode;
     }
 
-    public void setLoc(int loc) {
-        this.loc = loc;
+    public Set<TreeNode> getChildNodes() {
+        return childNodes;
     }
 
-    public void setCloc(int cloc) {
-        this.cloc = cloc;
+    public String getPath() {
+        return path;
     }
 
-    public void setWmc(int wmc) {
-        this.wmc = wmc;
+    public int getLoc() {
+        return loc;
     }
 
-    public void setDc(float dc) {
-        this.dc = dc;
+    public int getCloc() {
+        return cloc;
     }
 
-    public void setBc(float bc) {
-        this.bc = bc;
+    public int getWmcwcp() {
+        return wmcwcp;
     }
 
-    public void setWcp(int wcp) {
-        this.wcp = wcp;
+    public float getDc() {
+        return dc;
+    }
+
+    public float getBc() {
+        return bc;
     }
 }
-
-/*
-
-Dans une node il y aura :
-- Des attributs pour chacun des métriques : mesurés et calculés
-- Un attribut path ou path relatif (à déterminer ce qu'on a besoin)
-- Une liste de node-enfants (comme ce n'est pas un arbre binaire)
-- Un lien vers la node parent
-- et des méthodes setters
-
-Les setters sur les attributs à cumuler s'appliqueront en remontant l'arbre.
-
-class in Java:
-
-    // Attributes
-
-    // Constructor
-
-    // Methods
-
-    // Getters and setters
-
- */
-
 
